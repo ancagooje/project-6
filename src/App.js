04db5705d45de7d9232923f6ab9c7c112f9d043f
Anca Gooje <ancagooje@gmail.com>
2018-10-18 12:10:17 -0400

    Added change shelf functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react'
-
 // import * as BooksAPI from './BooksAPI'
-import './App.css';
-
-import { Route} from 'react-router-dom'
-
-import MainPage from './components/pages/MainPage';
-import SearchPage from './components/pages/SearchPage';
+import './App.css'
+import BookCase from './components/BookCase';
 
 class BooksApp extends React.Component {
-   
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false
+  }
 
   render() {
     return (
-      <div>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/" component={SearchPage} />
-        </div>
-    );
- } 
-}  
-             
+      <BookCase />
+    )
+  }
+}
+
 export default BooksApp
 /**
      * TODO: Instead of using this state variable to keep track of which page
